bbf4ff0e23af6a33329ac6ad571f289439683907
Kallam <kallamsamad@gmail.com>
2026-03-15 05:03:58 +0000

    Created basic html skeleton

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kallam Samad</title>
+</head>
+<body>
+
+<div class="page">
+    <div class="header">
+         <div class="headertop"></div>
+        <div class="headermiddle"></div>
+    </div>
+    <div class="main">
+        <div class="maintop"></div>
+        <div class="mainmiddle"></div>
+        <div class="mainbottom"></div>
+    </div>
+    <div class="footer">
+
+    </div>
+</div>
+
+
+
+
+</body>
+</html>
